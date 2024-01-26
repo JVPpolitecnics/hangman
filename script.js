7e3988e012822bfd3944456bcdb176fa5c7892ab
David <dnerizb2223@politecnics.barcelona>
2024-01-26 17:58:12 +0100

    almost

--- script.js
+++ script.js
@@ -24,11 +24,10 @@ let coincidence = null;
 let foundLetterArray = [];
 let randomWord = null;
 let successCounter = 0;
-let randomIndex = Math.floor(Math.random() * 20);
-let explanation = words[randomIndex][1];
+let explanation ;
 let clickableLetterArray = [];
 let alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("");
-
+let randomIndex;
 function checkName() {
   let username = document.getElementById("name").value;
 
@@ -40,15 +39,19 @@ function checkName() {
 }
 
 function startGame() {
+  randomIndex = Math.floor(Math.random() * 20);
+  foundLetterArray = [];
   let initialMessageBox = document.getElementById("initialMessageBox");
-
+if (initialMessageBox != null){
   initialMessageBox.remove();
+}
+  explanation = words[randomIndex][1];
   randomWord = words[randomIndex][0];
 executeGameFunctionality();
 }
 
 function executeGameFunctionality(){
-  addHintToGame();
+  addHintToGame(true);
   addMaskedWords();
   addLetterButtons();
   showLetterButtons();
@@ -58,6 +61,11 @@ function executeGameFunctionality(){
 function checkCompleteWord() {
   if (foundLetterArray.length == randomWord.length) {
     successCounter++;
+    let points = document.getElementById("points");
+    points.innerHTML = successCounter;
+    return true;
+  } else {
+    return false;
   }
 }
 
@@ -65,7 +73,10 @@ function actUpponCoincidence() {
   if (coincidence != null) {
     foundLetterArray.push(coincidence);
     updateBlanksForFoundLetter();
-    checkCompleteWord();
+     let success = checkCompleteWord();
+     if (success){
+ addHintToGame(false);
+     }
   }
 }
 
@@ -87,11 +98,32 @@ function updateBlanksForFoundLetter() {
   });
 }
 
-function addHintToGame() {
-  let hint = document.createElement("h1");
+function addHintToGame(firstExecution) {
+  let hint;
+  let points;
+  if (firstExecution){
+    hint = document.createElement("h1");
+    hint.id = "hint";
   hint.innerHTML = explanation;
   hint.className = "gameArea";
+
+   points = document.createElement("h1");
+  points.innerHTML = successCounter;
+  points.id = "points";
+  document.body.appendChild(points);
   document.body.appendChild(hint);
+  } else {
+    randomIndex =  Math.floor(Math.random() * 20);
+    explanation = words[randomIndex][1];
+    randomWord = words[randomIndex][0];
+hint = document.getElementById("hint");
+points = document.getElementById("points");
+hint.innerHTML = explanation;
+let wordArea = document.getElementById("wordArea");
+wordArea.remove();
+addMaskedWords();
+  }
+
 }
 
 function checkIfLetterCoincides() {
