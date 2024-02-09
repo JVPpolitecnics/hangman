8a1ffac3d0c7bddc502ce9fd6065b2d435bc2f61
David <dnerizb2223@politecnics.barcelona>
2024-02-09 17:44:29 +0100

    black

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let words = [
-  ["apple", "Fruit used for cider making", "apple.jpg"],
-  ["banana", "Fruit that has a variety named 'Cavendish'", "banana.jpg"],
-  ["chair", "Furniture", "chair.jpg"],
-  ["dog", "Pet", "dog.jpg"],
-  ["elephant", "Animal with a trunk", "elephant.jpg"],
-  ["flower", "Associated with plants reproducing", "flower.jpg"],
-  ["grape", "Fruit used to make wine", "grape.jpg"],
-  ["horse", "Animal", "horse.jpg"],
-  ["igloo", "Shelter", "igloo.jpg"],
-  ["jacket", "Clothing", "jacket.jpg"],
-  ["kangaroo", "Marsupilanian", "kangaroo.jpg"],
-  ["lion", "Animal you see in a safari reserve", "lion.jpg"],
-  ["monkey", "Primate", "monkey.jpg"],
-  ["nose", "Part of the body", "nose.jpg"],
-  ["orange", "Citrus", "orange.jpg"],
-  ["penguin", "Bird", "penguin.jpg"],
-  ["quilt", "Cover", "quilt.jpg"],
-  ["rabbit", "Rodent", "rabbit.jpg"],
+  ["apple", "Fruit used for cider making", "apple.png"],
+  ["banana", "Fruit that has a variety named 'Cavendish'", "banana.png"],
+  ["chair", "Furniture", "chair.png"],
+  ["dog", "Pet", "dog.png"],
+  ["elephant", "Animal with a trunk", "elephant.png"],
+  ["flower", "Associated with plants reproducing", "flower.png"],
+  ["grape", "Fruit used to make wine", "grape.png"],
+  ["horse", "Animal", "horse.png"],
+  ["igloo", "Shelter", "igloo.png"],
+  ["jacket", "Clothing", "jacket.png"],
+  ["kangaroo", "Marsupilanian", "kangaroo.png"],
+  ["lion", "Animal you see in a safari reserve", "lion.png"],
+  ["monkey", "Primate", "monkey.png"],
+  ["nose", "Part of the body", "nose.png"],
+  ["orange", "Citrus", "orange.png"],
+  ["penguin", "Bird", "penguin.png"],
+  ["quilt", "Cover", "quilt.png"],
+  ["rabbit", "Rodent", "rabbit.png"],
   ["sun", "Planet", "sun.jpg"],
-  ["tree", "Produces oxygen during the day", "tree.jpg"],
+  ["tree", "Produces oxygen during the day", "tree.png"],
 ];
 let coincidenceCounter;
 let foundLetterArray = [];
@@ -34,7 +34,7 @@ let points = parseInt(localStorage.getItem('points')) || 0;
 
 
 function checkName() {
-  let username = document.getElementById("name").value;
+  let username = document.getElementById("name").value || localStorage.getItem('name');
 
   if (username == "") {
     alert("Please fill in your name");
@@ -48,15 +48,34 @@ function startGame(update) {
   randomIndex = Math.floor(Math.random() * 20);
   randomWord = words[randomIndex][0];
   explanation = words[randomIndex][1];
+  imgPath =  words[randomIndex][2];
   foundLetterArray = [];
   let initialMessageBox = document.getElementById("initialMessageBox");
   if (initialMessageBox != null) {
     initialMessageBox.remove();
+    addLogOutButton()
   }
   addMaskedWords(update);
   createLetterButtons(update);
-  addHintToGame(update);
+  addHintPointsAndImagesToGame(update);
   updateImgHangMan(mistake);
+}
+
+function addLogOutButton(){
+  let button = document.createElement('button');
+  button.className = "button";
+  button.id = "logout";
+  button.innerHTML = "Log-out"
+  button.addEventListener("click", function () {
+    logOut();
+  });
+  document.body.appendChild(button);
+
+}
+
+function logOut(){
+  localStorage.clear();
+  window.location.reload();
 }
 
 //function in which most of the game functionality is handled
@@ -83,7 +102,7 @@ function handleClick(event) {
 }
 function updatePointsPanel(){
   let point = document.getElementById("points");
-  point.innerHTML = points;
+  point.innerHTML = "points: " + points;
 }
 function updateImgHangMan(mistake) {
   let hangmanImg = document.getElementById("hangmanImg");
@@ -115,24 +134,32 @@ function updateBlanksForFoundLetter() {
   });
 }
 
-function addHintToGame(update) {
+function addHintPointsAndImagesToGame(update) {
   let hint;
   let pointBox;
+  let imgBox;
   if (!update) {
     hint = document.createElement("h1");
     hint.id = "hint";
     hint.className = "gameArea";
     pointBox = document.createElement("h1");
     pointBox.id = "points";
+    imgBox = document.createElement("img");
+    imgBox.id = "imgBox";
+    imgBox.src = "img/words/" + imgPath;
+    let container = document.getElementById("imgBoxContainer");
+    container.appendChild(imgBox);
     document.body.appendChild(pointBox);
     document.body.appendChild(hint);
   } else {
     hint = document.getElementById("hint");
     pointBox = document.getElementById("points");
+    imgBox = document.getElementById("imgBox");
+    imgBox.src = "img/words/" + imgPath;
   }
 
   hint.innerHTML = explanation;
-  pointBox.innerHTML = points;
+  pointBox.innerHTML = "points: " +points;
 }
 
 function addMaskedWords(update) {
@@ -172,6 +199,6 @@ window.addEventListener('beforeunload', function() {
   // Save data to localStorage
   localStorage.setItem('points', points);
   localStorage.setItem('mistake', mistake);
-  localStorage.setItem('name', mistake);
   localStorage.setItem('wordIndex', randomIndex);
+  localStorage.setItem('name', username);
 });
